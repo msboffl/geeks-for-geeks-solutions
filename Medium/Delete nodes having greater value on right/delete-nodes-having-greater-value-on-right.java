091bf0c86207f22dacf396b1c37d8f733631cbba
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2023-08-29 07:27:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Delete nodes having greater value on right/delete-nodes-having-greater-value-on-right.java
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node compute(Node head)
+    {
+        min= Integer.MIN_VALUE;
+        
+        Node dummy=new Node(-1);
+        dummy.next=head;
+        
+        computeR(dummy, dummy.next);
+        
+        return dummy.next;
+        
+        
+    }
+    static int min;
+    
+    void computeR(Node prev, Node curr){
+        
+        if(curr.next==null){
+            min=Math.max(min,curr.data);
+            return;
+        }
+        
+        computeR(curr, curr.next);
+        
+        if(curr.data<min) prev.next=curr.next;
+        else min=Math.max(min,curr.data);// your code here
+    }
+}
+  
