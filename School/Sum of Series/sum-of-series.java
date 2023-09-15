3c81b3e5830e47bf8e564ccdeb0db1022794a12a
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2023-09-15 08:36:25 +0530

    Updated Solution - GfG to GitHub

--- School/Sum of Series/sum-of-series.java
+++ School/Sum of Series/sum-of-series.java
@@ -28,7 +28,7 @@ class GFG {
 class Solution {
     // function to return sum of  1, 2, ... n
     long seriesSum(int n) {
-        return (long) n * (n+1) / 2;
+        return (long) n * (n + 1) / 2;
         // code here
     }
 }
