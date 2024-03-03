e5371881d9c4b8c3704bfaf5bb6564e390661911
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2024-03-03 09:12:58 +0530

    Updated Solution - GfG to GitHub

--- Easy/Selection Sort/selection-sort.java
+++ Easy/Selection Sort/selection-sort.java
@@ -31,21 +31,18 @@ class GFG
 // } Driver Code Ends
 
 
-class Solution
-{
-	void selectionSort(int arr[], int n)
-	{
-	    for (int i = 0; i < n - 1; i++) {
-            int mini = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[mini]) {
-                    mini = j;
-                }
-            }
-            //swap
-            int temp = arr[mini];
-            arr[mini] = arr[i];
-            arr[i] = temp;
+class Solution {
+	void selectionSort(int arr[], int n) {
+	    for(int i = 0; i < n-1; i++) {
+	        int min = i;
+	        for(int j = i+1; j < n; j++) {
+	            if(arr[j] < arr[min]) {
+	                min = j;
+	            }
+	        }
+	        int temp = arr[min];
+	        arr[min] = arr[i];
+	        arr[i] = temp;
 	    }
 	    //code here
 	}
